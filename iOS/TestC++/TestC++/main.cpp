c685d99687ed6fe802270d192bfbc0ffaacdca48
Klaus Naumann <naumannk@googlemail.com>
2015-06-17 20:20:22 +0200

    structured directory

---
+++ iOS/TestC++/TestC++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestC++
+//
+//  Created by K on 16/06/15.
+//  Copyright © 2015 K. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
